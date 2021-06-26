40847b35794bb7d7f99bf926b59849c85bb86cf9
Santoshkushwaha-coder <souravsantosh1998@gmail.com>
2021-06-26 22:31:58 +0530

    Quinzy website page

---
+++ Internship Project/web.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.querySelector(".responsive");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+
+    var y = document.querySelector(".responsive1");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+    
+  }
